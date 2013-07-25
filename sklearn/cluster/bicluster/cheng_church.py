dd6a4bd395710a8f5fbab98f0ed67103e53fa1e5
Kemal Eren <kemal@kemaleren.com>
2013-07-25 12:01:28 -0400

    fixed some bugs

--- sklearn/cluster/bicluster/cheng_church.py
+++ sklearn/cluster/bicluster/cheng_church.py
@@ -27,12 +27,13 @@ class ChengChurch(six.with_metaclass(ABCMeta, BaseEstimator,
                                      BiclusterMixin)):
     def __init__(self, n_clusters=3, max_msr=1, deletion_threshold=1,
                  row_deletion_cutoff=100, column_deletion_cutoff=100,
-                 random_state=None):
+                 inverse_rows=True, random_state=None):
         self.n_clusters = n_clusters
         self.max_msr = max_msr
         self.deletion_threshold = deletion_threshold
         self.row_deletion_cutoff = row_deletion_cutoff
         self.column_deletion_cutoff = column_deletion_cutoff
+        self.inverse_rows = inverse_rows
         self.random_state = random_state
 
     def _check_parameters(self):
@@ -68,21 +69,21 @@ class ChengChurch(six.with_metaclass(ABCMeta, BaseEstimator,
             raise EmptyBiclusterException()
         msr_array = submatrix - row_mean[:, np.newaxis] - col_mean + mean
         msr = np.power(msr_array, 2).sum() / (n_rows * n_cols)
-        row_msr = np.power(msr_array, 2).sum(axis=1) / n_rows
-        col_msr = np.power(msr_array, 2).sum(axis=0) / n_cols
+        row_msr = np.power(msr_array, 2).sum(axis=1) / n_cols
+        col_msr = np.power(msr_array, 2).sum(axis=0) / n_rows
         return msr, row_msr, col_msr
 
     def _compute_inverse_row_msr(self, rows, cols, X):
         submatrix, row_mean, col_mean, mean = \
             self._precompute(rows, cols, X)
-        inverse_row_msr = -submatrix + row_mean - col_mean + mean
+        inverse_row_msr = \
+            -submatrix + row_mean[:, np.newaxis] - col_mean + mean
         inverse_row_msr = np.power(inverse_row_msr, 2).sum(axis=1)
         return inverse_row_msr / len(cols)
 
     def _node_deletion(self, rows, cols, X):
         msr, row_msr, col_msr = self._compute_msr(rows, cols, X)
-        n_rows = len(rows)
-        n_cols = len(cols)
+        n_rows, n_cols = len(rows), len(cols)
         while msr > self.max_msr:
             row_id = np.argmax(row_msr)
             col_id = np.argmax(col_msr)
@@ -93,15 +94,13 @@ class ChengChurch(six.with_metaclass(ABCMeta, BaseEstimator,
             if n_rows == len(rows) and n_cols == len(cols):
                 break
             else:
-                n_rows = len(rows)
-                n_cols = len(cols)
+                n_rows, n_cols = len(rows), len(cols)
             msr, row_msr, col_msr = self._compute_msr(rows, cols, X)
         return rows, cols
 
     def _multiple_node_deletion(self, rows, cols, X):
         msr, row_msr, col_msr = self._compute_msr(rows, cols, X)
-        n_rows = len(rows)
-        n_cols = len(cols)
+        n_rows, n_cols = len(rows), len(cols)
         while msr > self.max_msr:
             if n_rows >= self.row_deletion_cutoff:
                 to_remove = row_msr > self.deletion_threshold * msr
@@ -115,34 +114,30 @@ class ChengChurch(six.with_metaclass(ABCMeta, BaseEstimator,
                 rows, cols = self._node_deletion(rows, cols, X)
                 break
             else:
-                n_rows = len(rows)
-                n_cols = len(cols)
+                n_rows, n_cols = len(rows), len(cols)
             msr, row_msr, col_msr = self._compute_msr(rows, cols, X)
         return rows, cols
 
     def _node_addition(self, rows, cols, X):
         msr, row_msr, col_msr = self._compute_msr(rows, cols, X)
-        n_rows = len(rows)
-        n_cols = len(cols)
-        while msr > self.max_msr:
-            to_add = row_msr < msr
-            rows = np.setunion1d(rows, rows[to_add])
+        n_rows, n_cols = len(rows), len(cols)
+        while msr < self.max_msr:
+            to_add = np.nonzero(row_msr < msr)[0]
+            rows = np.union1d(rows, to_add)
 
             msr, row_msr, col_msr = self._compute_msr(rows, cols, X)
-            to_add = col_msr < msr
-            rows = np.setunion1d(cols, cols[to_add])
+            to_add = np.nonzero(col_msr < msr)[0]
+            cols = np.union1d(cols, to_add)
 
             if self.inverse_rows:
                 inverse_row_msr = self._compute_inverse_row_msr(rows, cols, X)
-                to_add = inverse_row_msr < msr
-                rows = np.setunion1d(rows, rows[to_add])
+                to_add = np.nonzero(inverse_row_msr < msr)[0]
+                rows = np.union1d(rows, to_add)
 
             if n_rows == len(rows) and n_cols == len(cols):
                 break
-            else:
-                n_rows = len(rows)
-                n_cols = len(cols)
             msr, row_msr, col_msr = self._compute_msr(rows, cols, X)
+            n_rows, n_cols = len(rows), len(cols)
         return rows, cols
 
     def _mask(self, X, rows, cols, generator, minval, maxval):
@@ -162,8 +157,7 @@ class ChengChurch(six.with_metaclass(ABCMeta, BaseEstimator,
 
         for i in range(self.n_clusters):
             try:
-                rows = np.arange(n_rows)
-                cols = np.arange(n_cols)
+                rows, cols = np.arange(n_rows), np.arange(n_cols)
                 rows, cols = self._multiple_node_deletion(rows, cols, X)
                 rows, cols = self._node_addition(rows, cols, X)
                 self._mask(X, rows, cols, generator, minval, maxval)

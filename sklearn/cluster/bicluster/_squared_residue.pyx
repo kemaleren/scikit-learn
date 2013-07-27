f2996f9a2ed1fd7c1ef07e96122dd7f495b84118
Kemal Eren <kemal@kemaleren.com>
2013-07-26 23:12:27 -0400

    compute all msr at once

--- sklearn/cluster/bicluster/_squared_residue.pyx
+++ sklearn/cluster/bicluster/_squared_residue.pyx
@@ -50,3 +50,53 @@ def squared_residue(np.ndarray[LONG, ndim=1, mode="c"] rows,
             val = (X[rows[i], cols[j]] - row_mean[i] - col_mean[j] + arr_mean)
             result[i, j] = val * val
     return result
+
+
+def all_msr(np.ndarray[LONG, ndim=1, mode="c"] rows,
+            np.ndarray[LONG, ndim=1, mode="c"] cols,
+            np.ndarray[DOUBLE, ndim=2, mode="c"] X):
+    cdef int n_rows = rows.shape[0]
+    cdef int n_cols = cols.shape[0]
+
+    cdef np.ndarray[DOUBLE, ndim = 1, mode = "c"] row_mean = None
+    cdef np.ndarray[DOUBLE, ndim = 1, mode = "c"] col_mean = None
+    row_mean = np.zeros(n_rows, dtype=np.float, order="c")
+    col_mean = np.zeros(n_cols, dtype=np.float, order="c")
+    cdef double arr_mean = 0.0
+
+    cdef double val = 0.0
+
+    for i in range(n_rows):
+        for j in range(n_cols):
+            val = X[rows[i], cols[j]]
+            row_mean[i] += val
+            col_mean[j] += val
+            arr_mean += val
+
+    for i in range(n_rows):
+        row_mean[i] /= n_cols
+    for j in range(n_cols):
+        col_mean[j] /= n_rows
+    arr_mean /= (n_rows * n_cols)
+
+    cdef np.ndarray[DOUBLE, ndim = 1, mode = "c"] row_msr = None
+    cdef np.ndarray[DOUBLE, ndim = 1, mode = "c"] col_msr = None
+    row_msr = np.zeros(n_rows, dtype=np.float, order="c")
+    col_msr = np.zeros(n_cols, dtype=np.float, order="c")
+
+    cdef double msr = 0.0
+
+    for i in range(n_rows):
+        for j in range(n_cols):
+            val = (X[rows[i], cols[j]] - row_mean[i] - col_mean[j] + arr_mean)
+            val = val * val
+            row_msr[i] += val
+            col_msr[j] += val
+            msr += val
+    for i in range(n_rows):
+        row_msr[i] /= n_cols
+    for j in range(n_cols):
+        col_msr[j] /= n_rows
+    msr /= (n_rows * n_cols)
+
+    return msr, row_msr, col_msr

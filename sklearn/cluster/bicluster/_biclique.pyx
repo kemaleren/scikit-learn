44a6aae9280bb8862f7da2162c2562c92094ca8f
Kemal Eren <kemal@kemaleren.com>
2013-08-26 16:38:12 -0400

    Got HashSet working

--- sklearn/cluster/bicluster/_biclique.pyx
+++ sklearn/cluster/bicluster/_biclique.pyx
@@ -1,9 +1,5 @@
 # encoding: utf-8
-"""
-# cython: cdivision=True
-# cython: boundscheck=False
-# cython: wraparound=False
-"""
+# cython: profile=True
 from collections import defaultdict
 
 import numpy as np
@@ -13,12 +9,262 @@ cimport cython
 
 np.import_array()
 
+from libc.stdlib cimport malloc, free
 
-def find_pivot(nodes, long[:] degrees, long lim):
-    cdef int pivot = -1
-    cdef int pivot_degree = -1
-    cdef int degree
-    cdef int n
+cdef class Node:
+    cdef Node prev
+    cdef Node next
+    cdef long value
+
+    def __cinit__(self):
+        self.prev = None
+        self.next = None
+        self.value = -1
+
+    def __repr__(self):
+        entries = []
+        cdef Node n = self.next
+        while n.value != -1:
+            entries.append('Node({})'.format(n.value))
+            n = n.next
+        return " <-> ".join(entries)
+
+
+cdef class HashSet:
+    cdef long[:] data  # holds elements
+    cdef long capacity  # 2^bits
+    cdef long bits
+    cdef long cardinality
+
+    cdef long min_bits
+    cdef float upper
+    cdef float lower
+    cdef long empty
+    cdef long removed
+
+    cdef Node nodes
+    cdef Node iter_node
+
+    def __cinit__(self):
+        self.min_bits = 5
+        self.bits = self.min_bits
+        self.capacity = 2 ** self.bits
+        self.upper = 0.7
+        self.lower = 0.1
+        self.empty = (2 ** 32) - 1
+        self.removed = (2 ** 32) - 2
+
+        self.cardinality = 0
+
+        self.data = np.empty(self.capacity, dtype=np.int)
+        self.data[:] = self.empty
+
+        self._init_nodes()
+
+    def _init_nodes(self):
+        cdef Node begin = Node()
+        cdef Node end = Node()
+        begin.next = end
+        end.prev = begin
+        self.nodes = begin
+        self.iter_node = self.nodes
+
+    def __len__(self):
+        return self.cardinality
+
+    def __iter__(self):
+        self.iter_node = self.nodes.next
+        return self
+
+    def __next__(self):
+        cdef long val = self.iter_node.value
+        if val == -1:
+            raise StopIteration
+        self.iter_node = self.iter_node.next
+        return val
+
+    def __bool__(self):
+        return len(self) > 0
+
+    def _hash(self, long value):
+        """Knuth's multiplicative method"""
+        return (value * 2654435761) % self.capacity
+
+    def _find_posn(self, long value):
+        cdef long posn = self._hash(value)
+        cdef long entry = self.data[posn]
+        while (entry != self.empty):
+            if entry == value:
+                break
+            posn = (posn + 1) % self.capacity
+            entry = self.data[posn]
+        return posn
+
+    def add_node(self, value):
+        cdef Node n = Node()
+        n.value = value
+        n.prev = self.nodes
+        n.next = self.nodes.next
+        self.nodes.next.prev = n
+        self.nodes.next = n
+
+    def remove_node(self, long value):
+        cdef Node n = self.nodes.next
+        while n.value != -1:
+            if n.value == value:
+                break
+            n = n.next
+        if n.value == -1:
+            raise Exception()
+        n.prev.next = n.next
+        n.next.prev = n.prev
+
+    def add(self, long value, bint resize=1):
+        cdef long posn = self._find_posn(value)
+        if (self.data[posn] != self.empty) and (self.data[posn] != self.removed) and (self.data[posn] != value):
+            raise Exception()
+        if self.data[posn] != value:
+            self.data[posn] = value
+            self.cardinality += 1
+            self.add_node(value)
+            if resize:
+                self._resize()
+
+    def update(self, long[:] values):
+        cdef long val
+        for val in values:
+            self.add(val)
+
+    def remove(self, long value):
+        cdef long posn = self._find_posn(value)
+        if self.data[posn] == value:
+            self.data[posn] = self.removed
+            self.cardinality -= 1
+            self.remove_node(value)
+            self._resize()
+
+    def pop(self):
+        if not self:
+            raise IndexError('pop from empty set')
+        cdef long value = self.nodes.next.value
+        self.remove(value)
+        return value
+
+    def contains(self, long value):
+        cdef long posn = self._find_posn(value)
+        return self.data[posn] == value
+
+    def copy(self):
+        cdef HashSet result = HashSet()
+        cdef long val
+        for val in self:
+            result.add(val)
+        return result
+
+    def _resize(self):
+        if self.cardinality > <long>(self.capacity * self.upper):
+            self.bits += 1
+        elif self.cardinality < <long>(self.capacity * self.lower):
+            if self.bits <= self.min_bits:
+                return
+            self.bits -= 1
+        else:
+            return
+        cdef long[:] old_data = self.data
+        cdef long old_capacity = self.capacity
+
+        self.capacity = 2 ** self.bits
+        self.data = np.empty(self.capacity, dtype=np.int)
+        self.data[:] = self.empty
+
+        cdef Node n = self.nodes.next
+        self._init_nodes()
+        self.cardinality = 0
+        while n.value != -1:
+            self.add(n.value, 0)
+            n = n.next
+
+    def intersection(self, HashSet other):
+        cdef long val
+        cdef HashSet result = HashSet()
+        if self.capacity < other.capacity:
+            smaller = self
+            larger = other
+        else:
+            smaller = other
+            larger = self
+        for val in smaller:
+            if larger.contains(val):
+                result.add(val)
+        return result
+
+    def intersection_array(self, long[:] arr):
+        cdef long val
+        cdef HashSet result = HashSet()
+        for val in arr:
+            if self.contains(val):
+                result.add(val)
+        return result
+
+    def difference(self, HashSet other):
+        cdef long val
+        cdef HashSet result = HashSet()
+        for val in self:
+            if not other.contains(val):
+                result.add(val)
+        return result
+
+    def difference_array(self, long[:] other):
+        cdef long val
+        cdef HashSet result = self.copy()
+        for val in other:
+            result.remove(val)
+        return result
+
+    def __repr__(self):
+        return "HashSet({})".format([i for i in self])
+
+cpdef precompute_neighbors(long[:, :] X):
+    cdef long n_rows
+    cdef long n_cols
+    n_rows = X.shape[0]
+    n_cols = X.shape[1]
+
+    cdef long row
+    cdef long col
+    cdef dict neighbors
+    neighbors = {}
+    for row in range(n_rows):
+        neighbors[row] = set(np.nonzero(X[row])[0] + n_rows)
+    for col in range(n_cols):
+        neighbors[col + n_rows] = set(np.nonzero(X[:, col])[0])
+
+    second_neighbors = defaultdict(set)
+    for row in range(n_rows):
+        for col in neighbors[row]:
+            second_neighbors[row].update(neighbors[col])
+    for col in range(n_cols):
+        col = col + n_rows
+        for row in neighbors[col]:
+            second_neighbors[col].update(neighbors[row])
+
+    cdef long node
+    cdef dict all_neighbors
+    all_neighbors = {}
+    for node in neighbors:
+        all_neighbors[node] = neighbors[node] | second_neighbors[node]
+        all_neighbors[node].discard(node)
+
+    for node in all_neighbors:
+        all_neighbors[node] = np.array(list(all_neighbors[node]), dtype=np.int)
+    return all_neighbors
+
+
+cpdef find_pivot(HashSet nodes, long[:] degrees, long lim):
+    cdef long pivot = -1
+    cdef long pivot_degree = -1
+    cdef long degree
+    cdef long n
     for n in nodes:
         degree = degrees[n]
         if degree > pivot_degree:
@@ -27,3 +273,75 @@ def find_pivot(nodes, long[:] degrees, long lim):
         if degree == lim:
             break
     return pivot, pivot_degree
+
+
+def find_bicliques(long[:, :] X):
+    """Find all bicliques in a bipartite graph formed from array X.
+
+    The graph has m+n nodes, where X has shape (m, n). If X[i, j]
+    is nonzero, there is an edge between i and j.
+
+    The bicliques are enumerated by a modification of the
+    Bron-Kerbosch algorithm. This function is based on networkx's
+    implementation.
+
+    """
+    neighbors = precompute_neighbors(X)
+    degrees = np.zeros(len(neighbors), dtype=np.long)
+    for n in neighbors:
+        degrees[n] = neighbors[n].shape[0]
+    pivot = np.argmax(degrees)
+
+    candidates = HashSet()
+    candidates.update(np.arange(X.shape[0] + X.shape[1]))
+    small_candidates = candidates.difference_array(neighbors[pivot])
+    done = HashSet()
+    stack = []
+    biclique_so_far = []
+
+    while small_candidates or stack:
+        if small_candidates:
+            node = small_candidates.pop()
+        else:
+            # go to next in stack
+            candidates, done, small_candidates = stack.pop()
+            biclique_so_far.pop()
+            continue
+
+        # add next node to biclique
+        biclique_so_far.append(node)
+        candidates.remove(node)
+        done.add(node)
+        nn = neighbors[node]
+        new_candidates = candidates.intersection_array(nn)
+        new_done = done.intersection_array(nn)
+
+        # check if we have more to search
+        if not new_candidates:
+            if not new_done:
+                result = biclique_so_far[:]
+                if len(result) > 1:
+                    # found a biclique
+                    yield result
+            biclique_so_far.pop()
+            continue
+
+        n_new_candidates = len(new_candidates)
+        pivot_done, degree_done = find_pivot(new_done, degrees,
+                                             n_new_candidates)
+        if degree_done == n_new_candidates:
+            # shortcut: this part of tree already searched
+            biclique_so_far.pop()
+            continue
+        pivot_cand, degree_cand = find_pivot(new_candidates, degrees,
+                                             n_new_candidates - 1)
+        if degree_cand > degree_done:
+            pivot = pivot_cand
+        else:
+            pivot = pivot_done
+
+        # save search tree for later backout
+        stack.append((candidates, done, small_candidates))
+        candidates = new_candidates
+        done = new_done
+        small_candidates = candidates.difference_array(neighbors[pivot])
--- sklearn/cluster/bicluster/tests/test_bimax.py
+++ sklearn/cluster/bicluster/tests/test_bimax.py
@@ -2,13 +2,62 @@
 
 import numpy as np
 from sklearn.cluster.bicluster import BiMax
-from sklearn.cluster.bicluster.bimax import _precompute_neighbors
+from sklearn.cluster.bicluster._biclique import precompute_neighbors, find_pivot, HashSet
+
+
+def test_hashset():
+    s = HashSet()
+    s.add(3)
+    s.add(4)
+    assert s.contains(3)
+    s.remove(3)
+    assert not s.contains(3)
+
+    s1 = HashSet()
+    s2 = HashSet()
+    s1.add(1)
+    s1.add(2)
+    s2.add(2)
+    s2.add(3)
+
+    r = s1.intersection(s2)
+    assert r.contains(2)
+    assert not r.contains(1)
+    assert not r.contains(3)
+
+    r = s1.difference(s2)
+    assert r.contains(1)
+    assert not r.contains(2)
+    assert not r.contains(3)
+
+    s1.update(np.array([3, 4, 5], dtype=np.int))
+    assert s1.contains(3)
+    assert s1.contains(4)
+    assert s1.contains(5)
+    assert not s1.contains(6)
+
+    assert len(s1) == 5
+    val = s1.pop()
+    assert len(s1) == 4
+    assert not s1.contains(val)
+
+    for node in s1:
+        assert s1.contains(node)
+
+    e = HashSet()
+    if e:
+        assert False
+    e.add(1)
+    if e:
+        assert True
+    if not e:
+        assert False
 
 
 def test_neighbors():
-    data = np.zeros((20, 20), dtype=np.bool)
+    data = np.zeros((20, 20), dtype=np.int)
     data[0:10, 0:10] = 1
-    neighbors = _precompute_neighbors(data)
+    neighbors = precompute_neighbors(data)
     for i in range(40):
         if (0 <= i < 10) or (20 <= i < 30):
             assert len(neighbors[i]) == 19
@@ -16,8 +65,16 @@ def test_neighbors():
             assert len(neighbors[i]) == 0
 
 
+def test_find_pivot():
+    nodes = HashSet()
+    nodes.update(np.arange(10, dtype=np.int))
+    degrees = np.zeros((10,), dtype=np.int)
+    degrees[3] = 5
+    assert find_pivot(nodes, degrees, 10) == (3, 5)
+
+
 def test_bimax():
-    data = np.zeros((20, 20), dtype=np.bool)
+    data = np.zeros((20, 20), dtype=np.int)
     data[0:10, 0:10] = 1
     model = BiMax()
     model.fit(data)
